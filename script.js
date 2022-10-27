21fff6180765b33d3af911429e1eef887f1a9ccc
idubrovskaya <irina.dubrovskaya95@gmail.com>
2022-10-27 21:23:08 +0300

    feat:add files

---
+++ script.js
@@ -0,0 +1,15 @@
+let smoothJumpUp = function () {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    window.scrollBy(0, -50);
+    setTimeout(smoothJumpUp, 10);
+  }
+};
+
+window.onscroll = function () {
+  let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrolled > 100) {
+    document.getElementById('upbutton').style.display = 'block';
+  } else {
+    document.getElementById('upbutton').style.display = 'none';
+  }
+};
